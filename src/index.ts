d28ccade1d7b7b3edba36e84a1de7ce505c303cf
himanshusarma <himanshusarma074@gmail.com>
2024-10-05 21:02:56 +0530

    package.json changes

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ["https://thriving-jalebi-92822e.netlify.app/"]
+  origin: ["https://thriving-jalebi-92822e.netlify.app"]
 }))
 app.use(express.json());
 
